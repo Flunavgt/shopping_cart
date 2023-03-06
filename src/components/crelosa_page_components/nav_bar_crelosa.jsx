bad70491632ebfb05368f797a670517ba4e90b54
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2023-03-06 16:36:39 -0600

    Add bootstrap

--- src/components/crelosa_page_components/nav_bar_crelosa.jsx
+++ src/components/crelosa_page_components/nav_bar_crelosa.jsx
@@ -1,38 +1,29 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import * as routes from '../../pages/routes';
 import '../../styles/CrelosaPage-styles/NavBarCrelosa.css';
 import '../../styles/CrelosaPage-styles/VARIABLES.css';
 
-const NavBarCrelosa = () => {
-  const navigate = useNavigate();
+const NavBarCrelosa = () => (
+  <section className="Main_container">
+    <header className="top_line" />
 
-  const ChangePage = () => {
-    navigate(routes.store);
-  };
+    <section className="NavBarCrelosa_MainContainer">
+      <div className="caratula_logo_container">
+        <img src="https://crelosa.com/wp-content/uploads/2019/11/logo.jpg" alt="CRELOSA_LOGO" className="logo_main_img" />
+      </div>
 
-  return (
-    <section className="Main_container">
-      <header className="top_line" />
-
-      <section className="NavBarCrelosa_MainContainer">
-        <div className="caratula_logo_container">
-          <img src="https://crelosa.com/wp-content/uploads/2019/11/logo.jpg" alt="CRELOSA_LOGO" className="logo_main_img" />
-        </div>
-
-        <div className="buttons_links_container">
-          <button type="button" href="">INICIO</button>
-          <button type="button" onClick={ChangePage}>CRELOSA STORE</button>
-          <a type="button" href="#info_products">PRODUCTOS & SEVICIOS</a>
-          <a type="button" href="#quienes_somos?">¿QUIENES SOMOS?</a>
-          <a type="button" href="#asociados">SOCIOS</a>
-          <a type="button" href="#piedepagina">CONTACTO</a>
-        </div>
-
-      </section>
+      <div className="buttons_links_container">
+        <a className="btn" href="#/">INICIO</a>
+        <a className="btn" href={routes.store}>CRELOSA STORE</a>
+        <a className="btn" href="#info_products">PRODUCTOS & SEVICIOS</a>
+        <a className="btn" href="#quienes_somos?">¿QUIENES SOMOS?</a>
+        <a className="btn" href="#asociados">SOCIOS</a>
+        <a className="btn" href="#piedepagina">CONTACTO</a>
+      </div>
 
     </section>
-  );
-};
+
+  </section>
+);
 
 export default NavBarCrelosa;
