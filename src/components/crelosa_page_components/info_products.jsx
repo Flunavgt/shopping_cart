1e7e4a218b1283c9225b459a8f8f55bdbf2fc52f
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2023-03-06 10:46:22 -0600

    Specify infoproducts space

--- src/components/crelosa_page_components/info_products.jsx
+++ src/components/crelosa_page_components/info_products.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../../styles/CrelosaPage-styles/info_products.css';
 
 const InfoProducts = () => (
-  <div>
-    <Link to="/main">
-      <button type="submit" className="toStorebtn">
-        this is it
-      </button>
-    </Link>
-  </div>
+  <section className="Main_infoproducts_container">
+
+    <div className="buttons__infoproducts_container">
+      <Link to="/main">
+        <button type="submit" className="toStorebtn">
+          RADIOS
+        </button>
+      </Link>
+
+      <Link to="/main">
+        <button type="submit" className="toStorebtn">
+          ACCESORIOS
+        </button>
+      </Link>
+    </div>
+
+  </section>
 );
 
 export default InfoProducts;
