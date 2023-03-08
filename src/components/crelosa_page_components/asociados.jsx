56dd48d31be1d8ede5c60c90f1878f4959e2b57f
Federico Luna <fluna1973@yahoo.com>
2023-03-07 19:20:00 -0600

    add mision and vision

--- src/components/crelosa_page_components/asociados.jsx
+++ src/components/crelosa_page_components/asociados.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import kenwood from './images/kenwood.jpeg';
+import midland from './images/Midland.jpeg';
+import hytera from './images/hytera.png';
+import doriginal from './images/doriginal.jpeg';
+import panorama from './images/panorama.png';
 import '../../styles/CrelosaPage-styles/asociados.css';
 
 const Asociados = () => (
   <section id="asociados" className="Main_asociados_container">
-    <h1>SOCIOS</h1>
+    <div className="brands">
+      <img src={kenwood} alt="kenwood" />
+      <img src={midland} alt="midland" />
+      <img src={hytera} alt="hytera" />
+      <img src={doriginal} alt="doriginal" />
+      <img src={panorama} alt="doriginal" />
+    </div>
   </section>
 );
 
