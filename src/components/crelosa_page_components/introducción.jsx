6543f1c8318037691d37eed54d7d920677e561c8
Federico Luna <fluna1973@yahoo.com>
2023-03-09 09:40:11 -0600

    add styling to intro

--- src/components/crelosa_page_components/introducción.jsx
+++ src/components/crelosa_page_components/introducción.jsx
@@ -12,7 +12,10 @@ const Introducción = () => {
             type="video/mp4"
           />
         </video>
-        <h1 className="titleCompanyintro">CRELOSA</h1>
+        <h1 className="titleCompanyintro">
+          Líder en
+          <span>Comunicaciones</span>
+        </h1>
         <div className="infoCompany">
           <p>
             Somos el operador de radiocomunicación troncalizado digital más
