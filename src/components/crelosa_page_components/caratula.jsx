04994e4d9fb21294b3c91a1009930de5fe6fef97
Federico Luna <fluna1973@yahoo.com>
2023-03-09 00:13:51 -0600

    add coordinates to rep. points

--- src/components/crelosa_page_components/caratula.jsx
+++ src/components/crelosa_page_components/caratula.jsx
@@ -68,36 +68,235 @@ const Caratula = () => {
       shadowUrl: markShadowPng,
     });
 
-    Leafleat.marker([15.203717, -91.741847], {
+    const coordenadas = [[15.203717, -91.741847], [14.5453861, -90.1185611], [15.7835, -90.2308]];
+
+    Leafleat.polygon(coordenadas, { color: 'red' }).addTo(map);
+
+    Leafleat.marker([14.585798, -90.517950], {
       icon: customIcon,
-      title: 'Punto de repetición B',
+      title: 'Oficina de atención al cliente',
       draggable: false,
     })
       .addTo(map)
-      .bindPopup('Radio de cobertura: yyy qqq');
+      .bindPopup('Parquep de Crelosa continuo');
     // .openPopup();
 
-    Leafleat.marker([14.581142, -90.482566], {
-      icon: customIcon,
-      title: 'Punto de repetición A',
-      draggable: false,
-    })
-      .addTo(map)
-      .bindPopup('Radio de cobertura: xxx zzz');
-    // .openPopup();
-
-    Leafleat.marker([15.950688, -89.717274], {
+    Leafleat.marker([14.4666028, -90.7430111], {
       icon: Leafleat.icon({
         iconUrl: collegePng,
-        iconSize: [38, 38],
+        iconSize: [30, 30],
         iconAnchor: [20, 0],
       }),
-      title: 'DMR',
+      title: 'Volcán de Agua',
       draggable: false,
     })
       .addTo(map)
-      .bindPopup('Ejemplo');
+      .bindPopup('Volcán de Agua');
+
+    Leafleat.marker([14.8210556, -91.4074083], {
+      icon: Leafleat.icon({
+        iconUrl: collegePng,
+        iconSize: [30, 30],
+        iconAnchor: [20, 0],
+      }),
+      title: 'Parraxquin',
+      draggable: false,
+    })
+      .addTo(map)
+      .bindPopup('Parraxquin');
     // .openPopup();
+
+    Leafleat.marker([14.5453861, -90.1185611], {
+      icon: Leafleat.icon({
+        iconUrl: collegePng,
+        iconSize: [30, 30],
+        iconAnchor: [20, 0],
+      }),
+      title: 'Miramundo',
+      draggable: false,
+    })
+      .addTo(map)
+      .bindPopup('Miramundo');
+
+    Leafleat.marker([14.2086056, -89.7994306], {
+      icon: Leafleat.icon({
+        iconUrl: collegePng,
+        iconSize: [30, 30],
+        iconAnchor: [20, 0],
+      }),
+      title: 'YUPILTEPEQUE',
+      draggable: false,
+    })
+      .addTo(map)
+      .bindPopup('YUPILTEPEQUE');
+
+    Leafleat.marker([15.2465889, -89.1739417], {
+      icon: Leafleat.icon({
+        iconUrl: collegePng,
+        iconSize: [30, 30],
+        iconAnchor: [20, 0],
+      }),
+      title: 'TIPON',
+      draggable: false,
+    })
+      .addTo(map)
+      .bindPopup('TIPON');
+
+    Leafleat.marker([15.6720111, -88.6936583], {
+      icon: Leafleat.icon({
+        iconUrl: collegePng,
+        iconSize: [30, 30],
+        iconAnchor: [20, 0],
+      }),
+      title: 'Las Escobas',
+      draggable: false,
+    })
+      .addTo(map)
+      .bindPopup('Las Escobas');
+
+    Leafleat.marker([14.7918306, -91.5886833], {
+      icon: Leafleat.icon({
+        iconUrl: collegePng,
+        iconSize: [30, 30],
+        iconAnchor: [20, 0],
+      }),
+      title: '7 orejas',
+      draggable: false,
+    })
+      .addTo(map)
+      .bindPopup('7 orejas');
+
+    Leafleat.marker([15.3966167, -91.4323861], {
+      icon: Leafleat.icon({
+        iconUrl: collegePng,
+        iconSize: [30, 30],
+        iconAnchor: [20, 0],
+      }),
+      title: 'LA CAPELLANIA',
+      draggable: false,
+    })
+      .addTo(map)
+      .bindPopup('LA CAPELLANIA');
+
+    Leafleat.marker([14.7844917, -91.0266583], {
+      icon: Leafleat.icon({
+        iconUrl: collegePng,
+        iconSize: [30, 30],
+        iconAnchor: [20, 0],
+      }),
+      title: 'Tecpan',
+      draggable: false,
+    })
+      .addTo(map)
+      .bindPopup('Tecpan');
+
+    Leafleat.marker([14.6184361, -90.5163222], {
+      icon: Leafleat.icon({
+        iconUrl: collegePng,
+        iconSize: [30, 30],
+        iconAnchor: [20, 0],
+      }),
+      title: 'Centro Financiero',
+      draggable: false,
+    })
+      .addTo(map)
+      .bindPopup('Centro Financiero');
+
+    Leafleat.marker([14.6245, -90.6403083], {
+      icon: Leafleat.icon({
+        iconUrl: collegePng,
+        iconSize: [30, 30],
+        iconAnchor: [20, 0],
+      }),
+      title: 'Cerro Alux',
+      draggable: false,
+    })
+      .addTo(map)
+      .bindPopup('Cerro Alux');
+
+    Leafleat.marker([14.3916056, -90.6085861], {
+      icon: Leafleat.icon({
+        iconUrl: collegePng,
+        iconSize: [30, 30],
+        iconAnchor: [20, 0],
+      }),
+      title: 'Cerro Chino',
+      draggable: false,
+    })
+      .addTo(map)
+      .bindPopup('Cerro Chino');
+
+    Leafleat.marker([14.6421306, -89.3840556], {
+      icon: Leafleat.icon({
+        iconUrl: collegePng,
+        iconSize: [30, 30],
+        iconAnchor: [20, 0],
+      }),
+      title: 'Quetzaltepeque',
+      draggable: false,
+    })
+      .addTo(map)
+      .bindPopup('Quetzaltepeque');
+
+    Leafleat.marker([14.9057111, -91.8129528], {
+      icon: Leafleat.icon({
+        iconUrl: collegePng,
+        iconSize: [30, 30],
+        iconAnchor: [20, 0],
+      }),
+      title: 'Ixtagel',
+      draggable: false,
+    })
+      .addTo(map)
+      .bindPopup('Ixtagel');
+
+    Leafleat.marker([14.57155, -90.4757556], {
+      icon: Leafleat.icon({
+        iconUrl: collegePng,
+        iconSize: [30, 30],
+        iconAnchor: [20, 0],
+      }),
+      title: 'Monte Bello',
+      draggable: false,
+    })
+      .addTo(map)
+      .bindPopup('Monte Bello');
+
+    Leafleat.marker([14.6292583, -90.5619833], {
+      icon: Leafleat.icon({
+        iconUrl: collegePng,
+        iconSize: [30, 30],
+        iconAnchor: [20, 0],
+      }),
+      title: 'BRS',
+      draggable: false,
+    })
+      .addTo(map)
+      .bindPopup('BRS');
+
+    Leafleat.marker([15.3386389, -90.2751667], {
+      icon: Leafleat.icon({
+        iconUrl: collegePng,
+        iconSize: [30, 30],
+        iconAnchor: [20, 0],
+      }),
+      title: 'Xucaneb',
+      draggable: false,
+    })
+      .addTo(map)
+      .bindPopup('Xucaneb');
+
+    Leafleat.marker([15.6797667, -89.6678556], {
+      icon: Leafleat.icon({
+        iconUrl: collegePng,
+        iconSize: [30, 30],
+        iconAnchor: [20, 0],
+      }),
+      title: 'Marmolera',
+      draggable: false,
+    })
+      .addTo(map)
+      .bindPopup('Marmolera');
   }, []);
 
   return (
