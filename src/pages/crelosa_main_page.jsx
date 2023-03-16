383e6a603e0afa80492294925a476ba379ff12a5
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2023-03-16 15:13:50 -0600

    Add cover for mobil version

--- src/pages/crelosa_main_page.jsx
+++ src/pages/crelosa_main_page.jsx
@@ -1,21 +1,52 @@
+import { useEffect, useState } from 'react';
 import NavBarCrelosa from '../components/crelosa_page_components/nav_bar_crelosa';
 import Caratula from '../components/crelosa_page_components/caratula';
-// import InfoProducts from '../components/crelosa_page_components/info_products';
-// import QuieneSomos from '../components/crelosa_page_components/quienes_somos';
 import Asociados from '../components/crelosa_page_components/asociados';
 import PieDePagina from '../components/crelosa_page_components/pie_de_pagina';
 import Introducción from '../components/crelosa_page_components/introducción';
+import '../App.css';
+import logo from '../components/crelosa_page_components/images/Introduccion/CRELOSA-01.png';
 
-const CrelosaMainPage = () => (
-  <section className="crelosa_main_page_container">
-    <NavBarCrelosa />
-    <Introducción />
-    <Caratula />
-    {/* <InfoProducts /> */}
-    {/* <QuieneSomos /> */}
-    <Asociados />
-    <PieDePagina />
-  </section>
-);
+const CrelosaMainPage = () => {
+  const useScreenSize = () => {
+    const [width, setWidth] = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight);
+
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    };
+
+    useEffect(() => {
+      window.addEventListener('resize', handleResize);
+
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
+    return { width, height };
+  };
+
+  if (useScreenSize().width < 800) {
+    return (
+      <div className="Mens_Working_here">
+        <h1>Versión Mobil no disponible</h1>
+        <img src={logo} alt="Logo_Crelosa" />
+      </div>
+    );
+  }
+
+  return (
+    <section className="crelosa_main_page_container">
+      <NavBarCrelosa />
+      <Introducción />
+      <Caratula />
+      {/* <InfoProducts /> */}
+      {/* <QuieneSomos /> */}
+      <Asociados />
+      <PieDePagina />
+    </section>
+  );
+};
 
 export default CrelosaMainPage;
