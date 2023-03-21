f60cd618e1854ba6ddb7d8a1514c389dcab85a7e
Federico Luna <fluna1973@yahoo.com>
2023-03-21 00:28:27 -0600

    add css to title video

--- src/components/crelosa_page_components/youtube_video.jsx
+++ src/components/crelosa_page_components/youtube_video.jsx
@@ -20,13 +20,16 @@ import ReactPlayer from 'react-player';
 
 const YoutubeVideo = () => (
   <div className="youtube_container_video">
-    <div className="container">
-      <ReactPlayer
-        playing
-        url="http://www.youtube.com/live/NiVMbP2y0Ho?feature=share"
-        className="react-player"
-        controls={false}
-      />
+    <h1>Transmisión en directo Volcán de Fuego</h1>
+    <div className="videoTitleyt">
+      <div className="container">
+        <ReactPlayer
+          playing
+          url="http://www.youtube.com/live/NiVMbP2y0Ho?feature=share"
+          className="react-player"
+          controls={false}
+        />
+      </div>
     </div>
   </div>
 );
