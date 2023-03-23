05a7a66ae305f9837f176d5f3ba8bd672d694b85
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2023-03-22 21:24:21 -0600

    Fix linters errors

--- src/components/crelosa_page_components/youtube_video.jsx
+++ src/components/crelosa_page_components/youtube_video.jsx
@@ -2,22 +2,6 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
 
-// const YoutubeVideo = () => (
-//   <div className="youtube_container_video">
-//     <ReactPlayer
-//       playing
-//       url="https://www.youtube.com/live/NiVMbP2y0Ho?feature=share"
-//       className="react-player"
-//       loop
-//       muted
-//       width="45vw"
-//       height="55vh"
-//       alt=""
-//       controls={false}
-//     />
-//   </div>
-// );
-
 const YoutubeVideo = () => (
   <div className="youtube_container_video">
     <h1>Transmisión en directo Volcán de Fuego</h1>
